6323a32d0b95186015123c8480765a88a07abcd5
marcus-rk <marcus.rappenborg@gmail.com>
2023-12-06 11:13:38 +0100

    Login modal and create user modal now have same css.

--- public/js-src/user.js
+++ public/js-src/user.js
@@ -1,16 +1,28 @@
 const frontPageCreateUserButton = document.querySelector('#front-create-user');
-const backdropElement = document.querySelector('#backdrop');
+const frontPageLogInUserButton = document.querySelector('#front-log-in');
 
 // Create user modal elements here:
 const userModalElement = document.querySelector('#create-user-modal');
+const userModalBackdropElement = document.querySelector('#create-backdrop');
 const modalCreateUserButton = document.querySelector('#create-user');
 const modalCancelCreateUserButton = document.querySelector('#create-user-cancel');
-const modalCreateUserUsernameInput = document.querySelector('#create-user-modal #username');
-const modalCreatedUserEmailInput = document.querySelector('#create-user-modal #email');
-const modalCreateUserPasswordInput = document.querySelector('#create-user-modal #password');
+const modalCreateUserUsernameInput = document.querySelector('#create-username');
+const modalCreatedUserEmailInput = document.querySelector('#create-email');
+const modalCreateUserPasswordInput = document.querySelector('#create-password');
 const modalCreateUserBirthdateInput = document.querySelector('#create-user-modal #date-of-birth');
 
+// Log in user modal elements here:
+const loginModalElement = document.querySelector('#login-user-modal');
+const loginModalBackdropElement = document.querySelector('#login-backdrop');
+const modalLoginUserButton = document.querySelector('#login-user');
+const modalCancelLoginUserButton = document.querySelector('#login-cancel');
+const modalLoginUserNameInput = document.querySelector('#login-username-mail');
+const modalLoginUserPasswordInput = document.querySelector('#login-password');
+
 frontPageCreateUserButton.addEventListener('click', toggleCreateUserModal);
+frontPageLogInUserButton.addEventListener('click', toggleLogInUserModal);
+modalLoginUserButton.addEventListener('click', loginUser);
+modalCancelLoginUserButton.addEventListener('click', toggleLogInUserModal);
 modalCancelCreateUserButton.addEventListener('click', toggleCreateUserModal);
 modalCreateUserButton.addEventListener('click', createNewUser);
 
@@ -35,6 +47,7 @@ function createNewUser() {
     }).then(response => {
         if (response.ok) {
             console.log('User created successfully');
+            toggleCreateUserModal();
             // TODO: What to do after user creation?
         } else {
             console.error('Something went wrong:', response.statusText);
@@ -49,9 +62,20 @@ function createNewUser() {
     });
 }
 
+function loginUser() {
+
+}
+
 function toggleCreateUserModal() {
     modalCreateUserUsernameInput.value = '';
     modalCreateUserPasswordInput.value = '';
     userModalElement.classList.toggle('hidden');
-    backdropElement.classList.toggle('hidden');
+    userModalBackdropElement.classList.toggle('hidden');
+}
+
+function toggleLogInUserModal() {
+    modalLoginUserNameInput.value = '';
+    modalLoginUserPasswordInput.value = '';
+    loginModalElement.classList.toggle('hidden');
+    loginModalBackdropElement.classList.toggle('hidden');
 }

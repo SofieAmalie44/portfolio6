d05b18090308caea79f26bc0e812cb2505136c41
SofieAmalie44 <sofieamaliethorlund@gmail.com>
2023-12-06 13:04:47 +0100

    js and css updates from today (attributes and styling)

--- public/js-src/cafeoverview.js
+++ public/js-src/cafeoverview.js
@@ -8,22 +8,26 @@ fetch('/cafes')
         console.log(cafeArray)
         cafeArray.forEach(cafeObject => {
 
-
-            const cafeLi = document.createElement('li');
-            const spanHeader = document.createElement('span');
-            const attributeUl = document.createElement('ul');
-            const priceLevelLI = document.createElement('li');
-            const noiseLevelLI = document.createElement('li')
-            const avaliableWifiLI = document.createElement('li')
-            const offersFoodLI = document.createElement('li')
             const checkEmoji = "✅"
             const errorEmoji = "❌"
+            const cafeLi = document.createElement('li');
+            cafeLi.setAttribute("class", "cafeLi");
+            const spanHeader = document.createElement('span');
+            spanHeader.setAttribute("id", "spanCafeName");
+            const attributeUl = document.createElement('ul');
+            const priceLevelLI = document.createElement('li');
+            const noiseLevelLI = document.createElement('li');
+            const avaliableWifiLI = document.createElement('li');
+            const offersFoodLI = document.createElement('li');
+            const descriptionBox = document.createElement("span");
+            descriptionBox.setAttribute("class", "descriptionSpan")
 
             spanHeader.innerText =  cafeObject.cafe_name
             priceLevelLI.innerText = cafeObject.price_level;
             noiseLevelLI.innerText = "Noise level: " + cafeObject.noise_level;
-            avaliableWifiLI.innerText = cafeObject.avaliable_wifi
-            offersFoodLI.innerText = cafeObject.offer_food
+            avaliableWifiLI.innerText = cafeObject.avaliable_wifi;
+            offersFoodLI.innerText = cafeObject.offer_food;
+            descriptionBox.innerText = cafeObject.description;
 
 
             if (cafeObject.available_wifi === 1) {
@@ -59,9 +63,10 @@ fetch('/cafes')
 
             cafeLi.appendChild(spanHeader);
             attributeUl.appendChild(priceLevelLI);
-            attributeUl.appendChild(noiseLevelLI)
-            attributeUl.appendChild(avaliableWifiLI)
-            attributeUl.appendChild((offersFoodLI))
+            attributeUl.appendChild(noiseLevelLI);
+            attributeUl.appendChild(avaliableWifiLI);
+            attributeUl.appendChild((offersFoodLI));
+            cafeLi.appendChild(descriptionBox);
             cafeLi.appendChild(attributeUl);
             cafeUl.appendChild(cafeLi);
         });
